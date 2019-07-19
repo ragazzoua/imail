4e18f4ea8768ebd82ca7fe7a7451eb5e87117e96
ragazzoua <ragazzo@ukr.net>
2019-07-19 08:51:13 +0300

    first test created

--- src/test/java/ua/i/testing/groupCreationTest.java
+++ src/test/java/ua/i/testing/groupCreationTest.java
@@ -3,7 +3,10 @@ package ua.i.testing;
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,6 +25,7 @@ public class groupCreationTest {
         WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
     }
 
     @Test
@@ -34,7 +38,7 @@ public class groupCreationTest {
         driver.findElement(By.name("pass")).clear();
         driver.findElement(By.name("pass")).sendKeys("337774a");
         driver.findElement(By.xpath("//input[@type='submit' and @value='Войти']")).click();
-        driver.findElement(By.xpath("//li[@class='new'][1]")).click();
+        driver.findElement(By.xpath("//ul[@class='list_underlined']/li[3]/a")).click();
         driver.findElement(By.xpath("//p[@class='make_message']/a")).click();
         driver.findElement(By.xpath("//textarea[@id='to']")).click();
         driver.findElement(By.xpath("//textarea[@id='to']")).sendKeys("to@ukr.net");
@@ -43,7 +47,21 @@ public class groupCreationTest {
         driver.findElement(By.xpath("//textarea[@id='text']")).click();
         driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys("text");
         driver.findElement(By.xpath("//input[@name='save_in_drafts']")).click();
-        driver.findElement(By.xpath("//li[@class='new'][2]")).click();
+        driver.findElement(By.xpath("//ul[@class='list_underlined']/li[3]/a")).click();
 
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public static boolean isAlertPresent(FirefoxDriver driver) {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
 }
