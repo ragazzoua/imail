bf25b026375a3f7a8e0dfcc4cacb1d12cb0c1f5c
ragazzoua <ragazzo@ukr.net>
2019-07-19 09:32:50 +0300

    refactor - extract - parametr object

---
+++ src/test/java/ua/i/testing/EmailData.java
@@ -0,0 +1,25 @@
+package ua.i.testing;
+
+public class EmailData {
+    private final String emailAddress;
+    private final String subject;
+    private final String emailText;
+
+    public EmailData(String emailAddress, String subject, String emailText) {
+        this.emailAddress = emailAddress;
+        this.subject = subject;
+        this.emailText = emailText;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getEmailText() {
+        return emailText;
+    }
+}
--- src/test/java/ua/i/testing/groupCreationTest.java
+++ src/test/java/ua/i/testing/groupCreationTest.java
@@ -34,7 +34,7 @@ public class groupCreationTest {
     public void emailCreationTest() {
         goToDrafts();
         initEmailCreation();
-        fillEmailForm("to@ukr.net", "subject", "text");
+        fillEmailForm(new EmailData("to@ukr.net", "subject", "text"));
         submitEmailCreation();
         goToDrafts();
 
@@ -54,13 +54,13 @@ public class groupCreationTest {
         driver.findElement(By.xpath("//input[@name='save_in_drafts']")).click();
     }
 
-    private void fillEmailForm(String emailAddress, String subject, String emailText) {
+    private void fillEmailForm(EmailData emailData) {
         driver.findElement(By.xpath("//textarea[@id='to']")).click();
-        driver.findElement(By.xpath("//textarea[@id='to']")).sendKeys(emailAddress);
+        driver.findElement(By.xpath("//textarea[@id='to']")).sendKeys(emailData.getEmailAddress());
         driver.findElement(By.xpath("//input[@name='subject']")).click();
-        driver.findElement(By.xpath("//input[@name='subject']")).sendKeys(subject);
+        driver.findElement(By.xpath("//input[@name='subject']")).sendKeys(emailData.getSubject());
         driver.findElement(By.xpath("//textarea[@id='text']")).click();
-        driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys(emailText);
+        driver.findElement(By.xpath("//textarea[@id='text']")).sendKeys(emailData.getEmailText());
     }
 
     private void initEmailCreation() {
