034f30a418563ef07d658cf752b20627e6b2aa10
ragazzoua <ragazzo@ukr.net>
2019-07-24 08:02:04 +0300

    add several browsers

--- src/test/java/ua/i/testing/appmanager/ApplicationManager.java
+++ src/test/java/ua/i/testing/appmanager/ApplicationManager.java
@@ -3,8 +3,13 @@ package ua.i.testing.appmanager;
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.remote.BrowserType.IE;
 
 /**
  * created by FAMILY 21.07.2019
@@ -18,10 +23,19 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
 
 
-
     public void init() {
-        WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        driver = new ChromeDriver();
+        String browser = BrowserType.FIREFOX;
+        if (browser == BrowserType.FIREFOX) {
+            WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+            driver = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+            driver = new ChromeDriver();
+        } else (browser ==BrowserType.IE){
+            WebDriverManager.getInstance(DriverManagerType.IEXPLORER).setup();
+            driver = new InternetExplorerDriver();
+        }
+
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.i.ua/");
@@ -30,7 +44,6 @@ public class ApplicationManager {
         sessionHelper = new SessionHelper(driver);
         sessionHelper.login("ittest2", "337774a");
     }
-
 
 
     public void stop() {
