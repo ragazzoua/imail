49c4729da2b36746e73a6a5c864c55c4a498feca
ragazzoua <ragazzo@ukr.net>
2019-08-14 21:59:59 +0300

    assert equals without id

--- src/test/java/ua/i/testing/model/EmailData.java
+++ src/test/java/ua/i/testing/model/EmailData.java
@@ -3,28 +3,29 @@ package ua.i.testing.model;
 import java.util.Objects;
 
 public class EmailData {
-    private final String id;
+    //private final String id;
     private final String emailAddress;
     private final String subject;
     private final String emailText;
 
     public EmailData(String emailAddress, String subject, String emailText) {
-        this.id = null;
+        //this.id = null;
         this.emailAddress = emailAddress;
         this.subject = subject;
         this.emailText = emailText;
     }
 
-    public EmailData(String id, String emailAddress, String subject, String emailText) {
-        this.id = id;
-        this.emailAddress = emailAddress;
-        this.subject = subject;
-        this.emailText = emailText;
-    }
+//    public EmailData(String id,
+//                     String emailAddress, String subject, String emailText) {
+//        //this.id = id;
+//        this.emailAddress = emailAddress;
+//        this.subject = subject;
+//        this.emailText = emailText;
+//    }
 
-    public String getId() {
-        return id;
-    }
+//    public String getId() {
+//        return id;
+//    }
 
     public String getEmailAddress() {
         return emailAddress;
@@ -39,25 +40,24 @@ public class EmailData {
     }
 
     @Override
-    public String toString() {
-        return "EmailData{" +
-                "id='" + id + '\'' +
-                ", emailAddress='" + emailAddress + '\'' +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EmailData emailData = (EmailData) o;
-        return Objects.equals(id, emailData.id) &&
-                Objects.equals(emailAddress, emailData.emailAddress);
+        return Objects.equals(emailAddress, emailData.emailAddress);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, emailAddress);
+        return Objects.hash(emailAddress);
     }
+
+    @Override
+    public String toString() {
+        return "EmailData{" +
+                "emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+
 }
