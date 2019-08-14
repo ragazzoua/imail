3fe2b461542acc7f818633fe63273338d366123c
ragazzoua <ragazzo@ukr.net>
2019-08-14 23:13:25 +0300

    assert equals with id

--- src/test/java/ua/i/testing/model/EmailData.java
+++ src/test/java/ua/i/testing/model/EmailData.java
@@ -3,29 +3,28 @@ package ua.i.testing.model;
 import java.util.Objects;
 
 public class EmailData {
-    //private final String id;
+    private final String id;
     private final String emailAddress;
     private final String subject;
     private final String emailText;
 
     public EmailData(String emailAddress, String subject, String emailText) {
-        //this.id = null;
+        this.id = null;
         this.emailAddress = emailAddress;
         this.subject = subject;
         this.emailText = emailText;
     }
 
-//    public EmailData(String id,
-//                     String emailAddress, String subject, String emailText) {
-//        //this.id = id;
-//        this.emailAddress = emailAddress;
-//        this.subject = subject;
-//        this.emailText = emailText;
-//    }
+    public EmailData(String id, String emailAddress, String subject, String emailText) {
+        this.id = id;
+        this.emailAddress = emailAddress;
+        this.subject = subject;
+        this.emailText = emailText;
+    }
 
-//    public String getId() {
-//        return id;
-//    }
+    public String getId() {
+        return id;
+    }
 
     public String getEmailAddress() {
         return emailAddress;
@@ -44,19 +43,21 @@ public class EmailData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EmailData emailData = (EmailData) o;
-        return Objects.equals(emailAddress, emailData.emailAddress);
+        return Objects.equals(id, emailData.id) &&
+                Objects.equals(emailAddress, emailData.emailAddress);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(emailAddress);
+        return Objects.hash(id, emailAddress);
     }
 
     @Override
     public String toString() {
         return "EmailData{" +
-                "emailAddress='" + emailAddress + '\'' +
+                "id='" + id + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
                 '}';
     }
 
