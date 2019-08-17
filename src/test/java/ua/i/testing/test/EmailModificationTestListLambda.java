9dd8c9145ec9845ae35103b9095d432e5262a870
ragazzoua <ragazzo@ukr.net>
2019-08-17 14:46:42 +0300

    a little refactoring

--- src/test/java/ua/i/testing/test/EmailModificationTestListLambda.java
+++ src/test/java/ua/i/testing/test/EmailModificationTestListLambda.java
@@ -19,21 +19,25 @@ public class EmailModificationTestListLambda extends TestBase {
             app.getGroupHelper().createDraft(new EmailData("to0000@ukr.net", null, null));
         }
         List<EmailData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().initEmailModification(before.size() - 1);
+        int index = before.size() - 1;
+        EmailData data = new EmailData(before.get(index).getId(), "to2@ukr.net", null, null);
 
-        EmailData data = new EmailData(before.get(before.size() - 1).getId(), "to2@ukr.net", null, null);
-
-        app.getGroupHelper().fillEmailForm(data);
-        app.getGroupHelper().submitEmailCreation();
-        app.getNavigationHelper().goToDrafts();
+        modifyGroup(index, data);
         List<EmailData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         before.add(data);
         Comparator<? super EmailData> byId = (g1, g2) -> Integer.compare(Integer.parseInt(g1.getId()), Integer.parseInt(g2.getId()));
         before.sort(byId);
         after.sort(byId);
         Assert.assertEquals(before, after);
+    }
+
+    private void modifyGroup(int index, EmailData data) {
+        app.getGroupHelper().initEmailModification(index);
+        app.getGroupHelper().fillEmailForm(data);
+        app.getGroupHelper().submitEmailCreation();
+        app.getNavigationHelper().goToDrafts();
     }
 }
